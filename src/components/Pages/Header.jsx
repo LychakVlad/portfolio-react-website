10c14eb0a522fd46e33b41d6ffe2e4680ec89be5
Vladislav <lycakvladislav@gmail.com>
2023-04-12 19:23:29 -0400

    fix

--- src/components/Pages/Header.jsx
+++ src/components/Pages/Header.jsx
@@ -13,6 +13,7 @@ const Header = () => {
 
   const menuHandler = (e) => {
     setOpened(!opened);
+
     if (opened === false) {
       window.scrollLock.disableScrolling();
     } else {
@@ -23,10 +24,10 @@ const Header = () => {
   const links = ['Skills', 'Projects', 'Contact'];
 
   return (
-    <header data-fix="block">
+    <header className="fixed z-50" data-fix="block">
       <div
         data-fix="block"
-        className="fixed left-2/4 transition-color duration-300 -translate-x-2/4 h-[80px] flex justify-evenly container xl  mx-auto items-center  text-indigo-950 text-2xl font-poppins max-lg:px-10 max-lg:justify-between max-lg:mx-0  z-50 bg-neutral-100 dark:bg-indigo-950 shadow-[0_-60px_30px_100px_rgba(245,245,245,1)] dark:shadow-[0_-60px_30px_100px_rgba(30,27,75,1)]"
+        className="fixed left-2/4 transition-color duration-300 -translate-x-2/4 h-[80px] flex justify-evenly container xl  mx-auto items-center  text-indigo-950 text-2xl font-poppins max-lg:px-10 max-lg:justify-between max-lg:mx-0  z-50 bg-neutral-100 dark:bg-indigo-950 shadow-[0_-60px_30px_100px_rgba(245,245,245,1)] dark:shadow-[0_-60px_30px_100px_rgba(30,27,75,1)] "
       >
         <NavSectionBig links={links} />
         <div className="max-lg:hidden">
